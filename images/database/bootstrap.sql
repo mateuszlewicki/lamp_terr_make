b68ade74678d310a023628cafb9948bf92b5b1db
mateuszlewicki <41284623+mateuszlewicki@users.noreply.github.com>
2021-10-25 11:15:11 +0200

    Update bootstrap.sql

--- images/database/bootstrap.sql
+++ images/database/bootstrap.sql
@@ -3,23 +3,23 @@ FLUSH PRIVILEGES;
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION;
 
-CREATE USER IF NOT EXISTS 'mlewicki'@'localhost' IDENTIFIED BY 'mlewicki';
+CREATE USER IF NOT EXISTS 'user'@'localhost' IDENTIFIED BY 'pass';
 
-CREATE DATABASE IF NOT EXISTS mlewicki  CHARACTER SET utf8 COLLATE utf8_general_ci;
-GRANT ALL ON mlewicki.* TO 'mlewicki'@'%' IDENTIFIED BY 'mlewicki';
+CREATE DATABASE IF NOT EXISTS exampleDB CHARACTER SET utf8 COLLATE utf8_general_ci;
+GRANT ALL ON exampleDB.* TO 'user'@'%' IDENTIFIED BY 'pass';
 
-CREATE TABLE mlewicki.test (
+CREATE TABLE exampleDB.test (
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name varchar(20)
 );
 
-INSERT INTO mlewicki.test (name) 
+INSERT INTO exampleDB.test (name) 
 VALUES ('dummy1');
-INSERT INTO mlewicki.test (name) 
+INSERT INTO exampleDB.test (name) 
 VALUES ('dummy2');
-INSERT INTO mlewicki.test (name) 
+INSERT INTO exampleDB.test (name) 
 VALUES ('dummy3');
-INSERT INTO mlewicki.test (name) 
+INSERT INTO exampleDB.test (name) 
 VALUES ('dummy4');
-INSERT INTO mlewicki.test (name) 
+INSERT INTO exampleDB.test (name) 
 VALUES ('dummy5');
